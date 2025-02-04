f723129b348921cec4be30106f39d463f3cd7b5c
pouya2000 <76437351+pouya2000@users.noreply.github.com>
2025-02-04 19:34:56 +0330

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -4,24 +4,24 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Telegram:
-    API_ID = int(env.get("API_ID"))
-    API_HASH = str(env.get("API_HASH"))
-    BOT_TOKEN = str(env.get("BOT_TOKEN"))
-    OWNER_ID = int(env.get('OWNER_ID', '7978482443'))
-    WORKERS = int(env.get("WORKERS", "6"))  # 6 workers = 6 commands at once
-    DATABASE_URL = str(env.get('DATABASE_URL'))
-    UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "Telegram"))
-    SESSION_NAME = str(env.get('SESSION_NAME', 'FileStream'))
-    FORCE_SUB_ID = env.get('FORCE_SUB_ID', None)
-    FORCE_SUB = env.get('FORCE_UPDATES_CHANNEL', False)
+    API_ID = int(env.get("1245871"))
+    API_HASH = str(env.get("2632135253c83a179f226cb882c85870"))
+    BOT_TOKEN = str(env.get("7629733815:AAGo72jlqjJ6ltqajyITISTRdYmWHIOg8z8"))
+    OWNER_ID = int(env.get('6335197342', '142095602'))
+    WORKERS = int(env.get("WORKERS", "5"))  # 6 workers = 6 commands at once
+    DATABASE_URL = str(env.get('mongodb+srv://pouya_sk54as567zk:<db_password>@test001.alvnr.mongodb.net/?retryWrites=true&w=majority&appName=test001'))
+    UPDATES_CHANNEL = str(env.get('-1002387122765'))
+    SESSION_NAME = str(env.get('FileStream'))
+    FORCE_SUB_ID = env.get('-1002387122765')
+    FORCE_SUB = env.get('-1002387122765')
     FORCE_SUB = True if str(FORCE_SUB).lower() == "true" else False
     SLEEP_THRESHOLD = int(env.get("SLEEP_THRESHOLD", "60"))
     FILE_PIC = env.get('FILE_PIC', "https://graph.org/file/5bb9935be0229adf98b73.jpg")
     START_PIC = env.get('START_PIC', "https://graph.org/file/290af25276fa34fa8f0aa.jpg")
     VERIFY_PIC = env.get('VERIFY_PIC', "https://graph.org/file/736e21cc0efa4d8c2a0e4.jpg")
     MULTI_CLIENT = False
-    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", None))   # Logs channel for file logs
-    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", None))   # Logs channel for user logs
+    FLOG_CHANNEL = int(env.get("-1002387122765"))   # Logs channel for file logs
+    ULOG_CHANNEL = int(env.get("-1002387122765"))   # Logs channel for user logs
     MODE = env.get("MODE", "primary")
     SECONDARY = True if MODE.lower() == "secondary" else False
     AUTH_USERS = list(set(int(x) for x in str(env.get("AUTH_USERS", "")).split()))
